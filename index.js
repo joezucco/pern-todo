8eb1a470d5d512c013606d258d679de2b958a86f
Joe Zucco <josephzucco@gmail.com>
2021-12-08 17:11:52 -0500

    get all todos

--- index.js
+++ index.js
@@ -16,16 +16,24 @@ app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1)",
+      "INSERT INTO todo (description) VALUES($1) RETURNING * ",
       [description]
     );
-      res.json(newTodo)
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.log(err.message);
   }
 });
 
 //get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //get a todo
 
